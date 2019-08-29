d2ade91ed55c792eb67df67382aa643a93c5b9e4
Noah Schefer <nbschefer@gmail.com>
2019-08-29 13:51:56 -0400

    stack complete

---
+++ colt-steele-masterclass/section-21-stacks+queues.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  //O(1)
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    return ++this.size;
+  }
+
+  //O(1)
+  pop() {
+    if (!this.first) return null;
+    const removed = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    }
+    this.first = removed.next;
+    this.size--;
+    return removed;
+  }
+}
+
+const stack = new Stack();
+
+stack.push(0);
+stack.push(1);
+stack.push(2);
+stack.pop();
+stack.pop();
+console.log(stack);
